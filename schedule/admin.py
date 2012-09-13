eb944e3c5f6c8d5261b27fcc7fa3a43735da3d4b
Fei Tan <fei@argonemyth.com>
2012-09-13 17:57:03 +0400

    Added location in the Event model and admin.py.

--- schedule/admin.py
+++ schedule/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from schedule.forms import RuleForm
-
 from schedule.models import Calendar, Event, CalendarRelation, Rule
 
 class CalendarAdminOptions(admin.ModelAdmin):
@@ -10,6 +9,13 @@ class CalendarAdminOptions(admin.ModelAdmin):
 class RuleAdmin(admin.ModelAdmin):
     form = RuleForm
 
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'start', 'end', 'location') 
+    search_fields = ('title', 'location', 'description')
+    date_hierarchy = "created_on"
+    ordering = ("-start", )
+
 admin.site.register(Calendar, CalendarAdminOptions)
 admin.site.register(Rule, RuleAdmin)
-admin.site.register([Event, CalendarRelation])
+admin.site.register(Event, EventAdmin)
+admin.site.register(CalendarRelation)
